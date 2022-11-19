ebede7201d8c1009fbffa32b2773b766094a727e
Stephen Nurse <62474479+stephennurse1971@users.noreply.github.com>
2022-11-19 12:17:57 +0000

    migration file update

--- migrations/Version20221111072245.php
+++ migrations/Version20221111072245.php
@@ -20,14 +20,15 @@ final class Version20221111072245 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE chavey_down');
+
         $this->addSql('ALTER TABLE file_attachments DROP vendor, DROP amount, DROP serpentime_comments, DROP hmrc_comments, DROP receipt, DROP cash_or_debit, DROP barclays, DROP caxton');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE chavey_down (id INT AUTO_INCREMENT NOT NULL, date DATE DEFAULT NULL, vendor VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, amount DOUBLE PRECISION DEFAULT NULL, attachments JSON DEFAULT NULL, serpentime_comments VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, hmrc_comments VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, description VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, receipt VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, cash_or_debit VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, barclays DOUBLE PRECISION DEFAULT NULL, caxton DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+
+
         $this->addSql('ALTER TABLE file_attachments ADD vendor VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD amount DOUBLE PRECISION DEFAULT NULL, ADD serpentime_comments VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD hmrc_comments VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD receipt VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD cash_or_debit VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD barclays DOUBLE PRECISION DEFAULT NULL, ADD caxton DOUBLE PRECISION DEFAULT NULL');
     }
 }
