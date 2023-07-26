ea0d09b44149dfab57f5959feaabce9196874fc4
Stephen Nurse <62474479+stephennurse1971@users.noreply.github.com>
2023-07-26 15:58:59 +0300

    CV upload

--- src/Controller/CurriculumVitaeController.php
+++ src/Controller/CurriculumVitaeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\CurriculumVitae;
+use App\Entity\JpmIcHistory;
 use App\Form\CurriculumVitaeType;
+use App\Form\JpmIcHistoryType;
 use App\Repository\CurriculumVitaeRepository;
 use App\Repository\StaticTextRepository;
 use App\Repository\UserRepository;
@@ -11,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/cv")
@@ -20,29 +23,28 @@ class CurriculumVitaeController extends AbstractController
     /**
      * @Route("/", name="curriculum_vitae_index", methods={"GET"})
      */
-    public function index(CurriculumVitaeRepository $curriculumVitaeRepository,UserRepository $userRepository): Response
+    public function index(CurriculumVitaeRepository $curriculumVitaeRepository, UserRepository $userRepository): Response
     {
-       $candidates_Id = $curriculumVitaeRepository->distinctCandidate();
-       $candidates = [];
-       foreach($candidates_Id as $candidate)
-       {
+        $candidates_Id = $curriculumVitaeRepository->distinctCandidate();
+        $candidates = [];
+        foreach ($candidates_Id as $candidate) {
 
-           $candidates[] = $userRepository->find($candidate[1]);
-       }
+            $candidates[] = $userRepository->find($candidate[1]);
+        }
 
         return $this->render('curriculum_vitae/index.html.twig', [
             'curriculum_vitaes' => $curriculumVitaeRepository->findAll(),
             //'candidates_Id'=> $candidates_Id,
-            'candidates'=>$candidates
+            'candidates' => $candidates
         ]);
     }
-
 
 
     /**
      * @Route("/stephen_nurse", name="curriculum_vitae", methods={"GET"})
      */
-    public function indexIndividual(CurriculumVitaeRepository $curriculumVitaeRepository, UserRepository $userRepository, StaticTextRepository $staticTextRepository){
+    public function indexIndividual(CurriculumVitaeRepository $curriculumVitaeRepository, UserRepository $userRepository, StaticTextRepository $staticTextRepository)
+    {
 
 
         $user = $userRepository->findOneBy([
@@ -50,7 +52,7 @@ class CurriculumVitaeController extends AbstractController
         return $this->render('curriculum_vitae/cv.html.twig', [
             'curriculum_vitaes' => $curriculumVitaeRepository->findByCandidate($user),
             'candidate' => $user,
-            'static_text'=> $staticTextRepository->findAll()
+            'static_text' => $staticTextRepository->findAll()
         ]);
     }
 
@@ -113,12 +115,33 @@ class CurriculumVitaeController extends AbstractController
      */
     public function delete(Request $request, CurriculumVitae $curriculumVitae): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$curriculumVitae->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $curriculumVitae->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($curriculumVitae);
             $entityManager->flush();
         }
 
+        return $this->redirectToRoute('curriculum_vitae_index');
+    }
+
+
+    /**
+     * @Route("/update/cv", name="update_cv", methods={"GET","POST"})
+     */
+    public function uploadCV(Request $request): Response
+    {
+        if (isset($_FILES['file'])) {
+            $newFilename = 'CV - Stephen Nurse.docx';
+             $files = $this->getParameter('files_cv_directory') . "/" . $newFilename;
+             if(file_exists($files)){
+                 unlink($files);
+             }
+            $file_name = $_FILES['file']['name'];
+            $file_tmp = $_FILES['file']['tmp_name'];
+            $file_type = $_FILES['file']['type'];
+            $file_ext = explode('.', $_FILES['file']['name']);
+            move_uploaded_file($file_tmp, $this->getParameter('files_cv_directory') . "/" . $newFilename);
+        }
         return $this->redirectToRoute('curriculum_vitae_index');
     }
 }
