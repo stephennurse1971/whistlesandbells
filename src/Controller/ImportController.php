9ed56caf408d6cd347edad335419725e72df826a
Stephen Nurse <62474479+stephennurse1971@users.noreply.github.com>
2023-07-27 11:21:05 +0300

    user index

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -55,7 +55,7 @@ class ImportController extends AbstractController
                     }
                     if ($source == 'Grapevine') {
                         $userImportGrapevineService->importUser($newFilename);
-                        return $this->redirectToRoute('user_index_recruiters');
+                        return $this->redirectToRoute('user_index');
                     }
                 }
             }
