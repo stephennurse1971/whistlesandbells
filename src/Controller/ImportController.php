62cd5b6d30c83bcaff0c01c0bd5be7512315e564
Stephen Nurse <62474479+stephennurse1971@users.noreply.github.com>
2023-07-27 11:30:27 +0300

    env

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -55,7 +55,7 @@ class ImportController extends AbstractController
                     }
                     if ($source == 'Grapevine') {
                         $userImportGrapevineService->importUser($newFilename);
-                        return $this->redirectToRoute('user_index');
+                        return $this->redirectToRoute('user_role_index', ['role' => 'ROLE_RECRUITER']);
                     }
                 }
             }
@@ -66,6 +66,6 @@ class ImportController extends AbstractController
 
         }
 
-        return $this->redirectToRoute('user_index');
+        return $this->redirectToRoute('recruiters_index');
     }
 }
