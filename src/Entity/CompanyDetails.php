9a7ca91705bdcfb7e2d4607ea39bec3332898352
stephen nurse <nurse_stephen@hotmail.com>
2024-10-17 14:56:15 +0300

    compnaydetails

--- src/Entity/CompanyDetails.php
+++ src/Entity/CompanyDetails.php
@@ -31,7 +31,7 @@ class CompanyDetails
     private $companyEmail;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $companyTel;
 
